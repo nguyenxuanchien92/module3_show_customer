095a0332b6cbb5ed246d9a0964a12644870a3dde
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2021-01-04 09:21:06 +0700

    has displayed images but still error font vietnamese

--- src/main/java/ShowCustomersServlet.java
+++ src/main/java/ShowCustomersServlet.java
@@ -13,17 +13,19 @@ public class ShowCustomersServlet extends HttpServlet {
     private static List<Customer> listCustomer = new ArrayList<>();
 
     static {
-        listCustomer.add(new Customer("Hạnh", "09-09-2000", "Hà Nội", "resources/images/hanh.jpg"));
-        listCustomer.add(new Customer("Hồng", "09-09-2000", "Hà Nội", "resources/images/hong.jpg"));
-        listCustomer.add(new Customer("Nga", "09-09-2000", "Hà Nội", "resources/images/nga.jpg"));
-        listCustomer.add(new Customer("Liên", "09-09-2000", "Hà Nội", "resources/images/lien.jpg"));
-        listCustomer.add(new Customer("Trang", "09-09-2000", "Hà Nội", "resources/images/trang.jpg"));
-        listCustomer.add(new Customer("Vi", "09-09-2000", "Hà Nội", "resources/images/vi.jpg"));
-        listCustomer.add(new Customer("Hạ Anh", "09-09-2000", "Hà Nội", "resources/images/anh.jpg"));
+        listCustomer.add(new Customer("Hạnh", "09-09-2000", "Hà Nội", "images/hanh.jpg"));
+        listCustomer.add(new Customer("Hồng", "09-09-2000", "Hà Nội", "images/hong.jpg"));
+        listCustomer.add(new Customer("Nga", "09-09-2000", "Hà Nội", "images/nga.jpg"));
+        listCustomer.add(new Customer("Liên", "09-09-2000", "Hà Nội", "images/lien.jpg"));
+        listCustomer.add(new Customer("Trang", "09-09-2000", "Hà Nội", "images/trang.jpg"));
+        listCustomer.add(new Customer("Vi", "09-09-2000", "Hà Nội", "images/vi.jpg"));
+        listCustomer.add(new Customer("Hạ Anh", "09-09-2000", "Hà Nội", "images/anh.jpg"));
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        req.setCharacterEncoding("utf-8");
         req.setAttribute("listCustomer",listCustomer);
         req.getRequestDispatcher("showListCustomer.jsp").forward(req,resp);
     }
